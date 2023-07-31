f79de0eeb25019fc101c1ea7dee0311eafc5a034
Alex <sakalinski95@gmail.com>
2023-07-31 18:36:58 +0300

    Добавьте файлы проекта.

---
+++ LAB_2_WPF/Driver.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LAB_2_WPF
+{
+    public enum GENDER {male, female, variant};
+    public enum EYES { blue, green, brown, gray}
+    public class Driver: INotifyPropertyChanged, IDataErrorInfo
+    {
+        int number;
+        char license_class;
+        double hgt;
+        string name;
+        string adress;
+        GENDER gender;
+        EYES eyes;
+        DateTime dob, iss, exp;
+        bool donor;
+        string urlimage;
+
+        public Driver()
+        {
+        }
+
+        public string this[string columnName]
+        {
+            get 
+            {
+                string error = string.Empty;
+                switch (columnName)
+                {
+                    case "License_class":
+                        if (License_class < 'A' || License_class > 'E')
+                            error = "Неверная категория";
+                        break;
+                    case "Exp":
+                        if (Exp < DateTime.Now)
+                            error = "Закончен срок действия";
+                            break;
+                    case "Number":
+                        if (!Regex.IsMatch(Number.ToString(), "[A-Z][a-z][1-9]"))
+                            error = "Номер должен содержать только английские буквы и цифры";
+                        break;
+                    case "Dob":
+                        if (Dob < new DateTime(1900,01,01) || Dob >= DateTime.Now.AddYears(-16))
+                            error = "Некорректная дата рождения";
+                        break;
+                    case "Iss":
+                        if (Iss <= DateTime.Now.AddYears(-16) || Iss > DateTime.Now)
+                            error = "Некорректная дата выдачи";
+                        break;                    
+                }
+                return error;
+            }
+        }
+
+        public int Number
+        {
+            get => number;
+            set
+            {
+                number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+        public char License_class 
+        { 
+            get => license_class;
+            set
+            {
+                license_class = value;
+                OnPropertyChanged("License_class");
+            }
+        }
+        public double Hgt
+        {
+            get => hgt; set
+            {
+                hgt = value;
+                OnPropertyChanged("Hgt");
+            }
+        }
+        public string Name
+        {
+            get => name; set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Adress
+        {
+            get => adress; set
+            {
+                adress = value;
+                OnPropertyChanged("Adress");
+            }
+        }
+        public GENDER Gender
+        {
+            get => gender; set
+            {
+                gender = value;
+                OnPropertyChanged("Gender");
+            }
+        }
+        public EYES Eyes
+        {
+            get => eyes; set
+            {
+                eyes = value;
+                OnPropertyChanged("Eyes");
+            }
+        }
+        public DateTime Dob
+        {
+            get => dob; set
+            {
+                dob = value;
+                OnPropertyChanged("Dob");
+            }
+        }
+        public DateTime Iss
+        {
+            get => iss; set
+            {
+                iss = value;
+                OnPropertyChanged("Iss");
+            }
+        }
+        public DateTime Exp
+        {
+            get => exp; set
+            {
+                exp = value;
+                OnPropertyChanged("Exp");
+            }
+        }
+        public bool Donor
+        {
+            get => donor; set
+            {
+                donor = value;
+                OnPropertyChanged("Donor");
+            }
+        }
+        public string Urlimage
+        {
+            get => urlimage; set
+            {
+                urlimage = value;
+                OnPropertyChanged("Urlimage");
+            }
+        }
+
+        public string Error => throw new NotImplementedException();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        public override string? ToString() => $"№{Number} {License_class} from {Iss} to {Exp}. {Name} DOB({Dob}). " +
+            $"{Adress} Height {Hgt} {Gender} Color of eyes {Eyes} {(Donor ? "Donor" : "Not donor")}"
+            +$" {Urlimage}";
+    
+    }
+}
---
+++ LAB_2_WPF/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+
+namespace LAB_2_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Driver driver = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (EYES color in Enum.GetValues(typeof(EYES)))
+            {
+                comboBoxEyes.Items.Add(color);
+            }
+            NewDriver();
+            grid.DataContext = driver;
+        }
+
+        private void Button_Save_Click(object sender, RoutedEventArgs e)
+        {           
+            MessageBox.Show(driver.ToString());
+        }
+
+        private void NewDriver()
+        {
+            driver.Number = 2;
+            driver.License_class = 'A';
+            driver.Hgt = 170;
+            driver.Name = "Петр";
+            driver.Adress = "Могилев";
+            driver.Gender = GENDER.male;
+            driver.Eyes = EYES.green;
+            driver.Dob = new DateTime(1970, 10, 3);
+            driver.Iss = new DateTime(2015, 5, 20);
+            driver.Exp = new DateTime(2025, 5, 20);
+            driver.Donor = false;
+            driver.Urlimage = @"/images/Petya.jpg";            
+        }
+        private void Button_Load_Click(object sender, RoutedEventArgs e)
+        {
+            driver.Number = 1;
+            driver.License_class = 'B';
+            driver.Hgt = 180;
+            driver.Name = "Федя";
+            driver.Adress = "Минск";
+            driver.Gender = GENDER.variant;
+            driver.Eyes = EYES.blue;
+            driver.Dob = new DateTime(2000, 1, 10);
+            driver.Iss = new DateTime(2020, 3, 26);
+            driver.Exp = new DateTime(2030, 3, 26);
+            driver.Donor = true;
+            driver.Urlimage = @"/images/Photo.jpg";
+        }
+
+        private void Button_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            NewDriver();
+            textBoxName.Text = driver.Name;
+            textBoxNumber.Text = driver.Number.ToString();
+            textBoxAddress.Text = driver.Adress;
+            textBoxClass.Text = driver.License_class.ToString();
+            datePickerDOB.SelectedDate = driver.Dob;
+            datePickerISS.SelectedDate = driver.Iss;
+            datePickerEXP.SelectedDate = driver.Exp;
+            sliderHGT.Value = driver.Hgt;
+            checkBoxDonor.IsChecked = driver.Donor;
+            if (driver.Gender == GENDER.male) radioButtonMale.IsChecked = true;
+            if (driver.Gender == GENDER.female) radioButtonFemale.IsChecked = true;
+            if (driver.Gender == GENDER.variant) radioButtonVariant.IsChecked = true;
+            comboBoxEyes.SelectedValue = driver.Eyes;
+            image.Source = new BitmapImage(new Uri("images/None.jpg", UriKind.RelativeOrAbsolute));
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog dialog = new();
+            dialog.Filter = "Файлы (jpg)|*.jpg|Все файлы|*.*";
+            if (dialog.ShowDialog() == true)
+            {
+                driver.Urlimage = dialog.FileName;
+                image.Source = new BitmapImage(new Uri(driver.Urlimage, UriKind.RelativeOrAbsolute));
+            }
+
+        }
+    }
+}
